9576000652fbbc5facfa0e9ab0cd70536a9f7bd5
Chris Atwood <coachchris289@gmail.com>
2019-02-11 17:12:57 -0800

    longest substring without repeatin characters

--- heaps.js
+++ heaps.js
@@ -1,29 +1,38 @@
 console.log("starting")
 // console.log(5+5)
-
 var lengthOfLongestSubstring = function (s) {
     let seenLetters = {};
-    let max = 0;
-    let tempCount = 0
-    for (let i = 0; i < s.length; i++) {
-        console.log(seenLetters)
-        if (tempCount > max) max = tempCount
-        if (!seenLetters[s[i]]) {
-            tempCount += 1;
-            seenLetters[s[i]] = true
+    seenLetters[s[0]] = true;
+    let max = 1;
+    let tempCount = 1;
+    let front = 0;
+    let rear = 0;
+    while (front < s.length - 1) {
+        console.log("front, rear indices: ", front, rear)
+        front += 1
+        if (!seenLetters[s[front]]) {
+            //if the new target of front is not a dup
+            tempCount += 1
             if (tempCount > max) max = tempCount
+            seenLetters[s[front]] = true
         } else {
-            tempCount = 1;
-            seenLetters = {}
-            console.log(seenLetters)
-            seenLetters[s[i]] = true
-            if (tempCount > max) max = tempCount
-
+            // if the new front is a dup, we move the rear pointer
+            // console.log('into the else"')
+            let going = true
+            while (going) {
+                seenLetters[s[rear]] = false
+                tempCount -= 1
+                if (s[rear] === s[front]) going = false
+                rear += 1
+            }
+            // console.log("b4 else ends")
+            // console.log("front, rear indices: ", front, rear)
         }
-
+        console.log("max, front, rear, Tcount: ", max, front, rear, tempCount)
+        console.log(seenLetters)
     }
 
     return max
 };
 
-console.log(lengthOfLongestSubstring('dvdf'))
+console.log(lengthOfLongestSubstring("aab"));
