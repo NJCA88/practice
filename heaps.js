5a29a5b5e114c91524cfbad3911435a5045d468d
Chris Atwood <coachchris289@gmail.com>
2019-02-12 11:40:41 -0800

    good solution to biggest substring with no repeat values

--- heaps.js
+++ heaps.js
@@ -4,35 +4,28 @@ var lengthOfLongestSubstring = function (s) {
     let seenLetters = {};
     seenLetters[s[0]] = true;
     let max = 1;
-    let tempCount = 1;
     let front = 0;
     let rear = 0;
-    while (front < s.length - 1) {
-        console.log("front, rear indices: ", front, rear)
+    while (front < s.length - 1  ) {
         front += 1
         if (!seenLetters[s[front]]) {
             //if the new target of front is not a dup
-            tempCount += 1
-            if (tempCount > max) max = tempCount
+            // tempCount += 1
+            if ((front - rear) + 1 > max) max = (front - rear) + 1
             seenLetters[s[front]] = true
         } else {
             // if the new front is a dup, we move the rear pointer
-            // console.log('into the else"')
-            let going = true
-            while (going) {
+            while ( !(s[rear]=== s[front]) ) {
                 seenLetters[s[rear]] = false
-                tempCount -= 1
-                if (s[rear] === s[front]) going = false
                 rear += 1
             }
-            // console.log("b4 else ends")
-            // console.log("front, rear indices: ", front, rear)
-        }
-        console.log("max, front, rear, Tcount: ", max, front, rear, tempCount)
-        console.log(seenLetters)
-    }
+            rear += 1
 
+        }
+
+    }
     return max
 };
 
-console.log(lengthOfLongestSubstring("aab"));
+// console.log(lengthOfLongestSubstring("abcabcbb"));
+console.log(lengthOfLongestSubstring("bbbb"));
