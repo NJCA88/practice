668cb306a1ae03f6a2512591fec5e4022505738c
Chris Atwood <coachchris289@gmail.com>
2019-08-07 14:09:24 -0700

    add subsets into quicksort file

--- quicksort.js
+++ quicksort.js
@@ -12,13 +12,28 @@ const quickSort = function(arr){
             rightArr.push(arr[i])
         }
     }
-    console.log(pivotArr, leftArr, rightArr)
     // return quickSort(leftArr).concat(pivotArr.concat(quickSort(rightArr)))
     let left = quickSort(leftArr).concat(pivotArr)
     let right = quickSort(rightArr)
     return left.concat(right)
 }
 // 
-let arr = [5, 7, 12, 4, -5, 2]
+// let arr = [5, 7, 12, 4, -5, 2]
 // let arr = [1]
-console.log(quickSort(arr))
+// console.log(quickSort(arr))
+
+const subSets = function(arr){
+    if (arr.length === 0) return [arr]
+    let first = [arr.shift()]
+    console.log('arr to run subsets on is: ', arr)
+    let newArr = subSets(arr)
+    console.log('newArr is: ', newArr)
+    answer = newArr.slice(0)
+    for (let i=0; i< newArr.length; i++){
+        answer.push( newArr[i].concat(first) )
+        // answer.push( first)
+    }
+    return answer
+}
+let arr = [1, 2]
+console.log(subSets(arr))
