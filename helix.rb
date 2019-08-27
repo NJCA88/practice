2bd2686ed1673db40d5be0900027762c354e2e59
Chris Atwood <coachchris289@gmail.com>
2019-08-27 13:29:22 -0700

    coding challenge problems

---
+++ helix.rb
@@ -0,0 +1,60 @@
+def braces(values)
+    answer = []
+    values.each do |input|
+        answer.push(test_braces(input))
+    end
+    return answer
+end
+
+def test_braces(word)
+    braces = {'{'=>'}', '}'=>'{', '('=> ')', ')'=>'(', '['=>']', ']'=>'['}
+    p1 = 0
+    p2 = word.length - 1
+        p "p1, p2: ", p1, p2
+
+    while p1 < p2
+        #whle the pointers haven't yet met / crossed
+        # p "p1, p2: ", p1, p2
+        p "iterating over while loop"
+        # p "vals: ", word[p1], word[p2]
+        if braces[word[p1]] #if its a type of brace
+            # p "starting with: ", word[p2]
+                p "p2 is: ", p2
+            if word[p2] == braces[word[p1]]
+                p2 -=1
+                p1 +=1
+                next
+            end
+            while word[p2] != braces[word[p1]]
+                if p1 >= p2
+                    return "NO"
+                end
+            end
+            p1 += 1
+        else
+            p1 +=1
+        end
+        if braces[word[p2]] #if its a type of brace
+            if word[p1] == braces[word[p2]]
+                p2 -=1
+                p1 +=1
+                next
+            end
+            while word[p1] != braces[word[p2]]
+                p "bottom while: ", p1, p2
+                p1 += 1
+                if p1 >= p2
+                    p "ending bottom p1, p2: ", p1, p2
+                    return "NO"
+                end
+            end
+            p2 -=1
+        else
+            p2 -= 1
+        end
+    end
+    return "YES"
+end
+
+p test_braces( '{}[]()' )
+
--- longest_string_chain.rb
+++ longest_string_chain.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Strings_hash
     attr_accessor :words
     def initialize(words)
@@ -7,8 +9,8 @@ end
 
 my_hash = Strings_hash.new(['cat', 'dog', 'kangaroo'])
 
-p 'hash is: ', my_hash
-p my_hash.words
+# p 'hash is: ', my_hash
+# p my_hash.words
 # descending_length = (a, b) {a.length <=> b.length})
 
 arr = ['cat', 'ca', 'bate', 'ratel', 'a']
@@ -16,3 +18,23 @@ arr = ['cat', 'ca', 'bate', 'ratel', 'a']
 # p "sorted: ",  arr.sort( & descending_length)
 # p arr.sort_by { |a| !a.length }
 # p arr
+
+def peak_index_in_mountain_array(a)
+    # a.each_with_index do |el, idx|
+    #     return idx if el> a[idx+1]
+    # end
+    # return arr.length /2
+    p "array is: ", a
+    mid = a.length / 2 
+
+    if (a[mid] > a[mid-1]) && (a[mid] > a[mid+1] )
+        return mid
+    end
+    if a[mid] < a[mid-1]
+        return peak_index_in_mountain_array( a.slice(0, mid + 1))
+    elsif a[mid] < a[mid+1]
+        return peak_index_in_mountain_array(a.slice(mid, a.length)) + mid
+    end
+end
+
+p peak_index_in_mountain_array([0,2, 1,0])
