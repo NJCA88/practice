0c0a41c6d9cefc4324bb2dba78da5a2e1faff7e8
Chris Atwood <coachchris289@gmail.com>
2019-08-30 22:39:08 -0700

    remove commented code

--- longest_string_chain.rb
+++ longest_string_chain.rb
@@ -9,21 +9,12 @@ end
 
 my_hash = Strings_hash.new(['cat', 'dog', 'kangaroo'])
 
-# p 'hash is: ', my_hash
-# p my_hash.words
-# descending_length = (a, b) {a.length <=> b.length})
 
 arr = ['cat', 'ca', 'bate', 'ratel', 'a']
 
-# p "sorted: ",  arr.sort( & descending_length)
-# p arr.sort_by { |a| !a.length }
-# p arr
+
 
 def peak_index_in_mountain_array(a)
-    # a.each_with_index do |el, idx|
-    #     return idx if el> a[idx+1]
-    # end
-    # return arr.length /2
     p "array is: ", a
     mid = a.length / 2 
 
